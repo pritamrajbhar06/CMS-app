0f5c419742ce8ed61da10cebf1acbaac7ba6ae23
Pritam Rajbhar <pritamrajbhar2001@gmail.com>
2025-07-14 00:38:43 +0530

    middlware added for categories

--- routes/api.php
+++ routes/api.php
@@ -10,11 +10,15 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
     // Category routes
+    Route::middleware('role:admin')->group(function () {
+        Route::post('/categories', [\App\Http\Controllers\Api\CategoryController::class, 'store']);
+        Route::get('/categories/{id}', [\App\Http\Controllers\Api\CategoryController::class, 'show']);
+        Route::put('/categories/{id}', [\App\Http\Controllers\Api\CategoryController::class, 'update']);
+        Route::delete('/categories/{id}', [\App\Http\Controllers\Api\CategoryController::class, 'destroy']);
+    });
+
+    // Author can just access categories
     Route::get('/categories', [\App\Http\Controllers\Api\CategoryController::class, 'index']);
-    Route::post('/categories', [\App\Http\Controllers\Api\CategoryController::class, 'store']);
-    Route::get('/categories/{id}', [\App\Http\Controllers\Api\CategoryController::class, 'show']);
-    Route::put('/categories/{id}', [\App\Http\Controllers\Api\CategoryController::class, 'update']);
-    Route::delete('/categories/{id}', [\App\Http\Controllers\Api\CategoryController::class, 'destroy']);
 
     // Article routes
     Route::get('/articles', [ArticleController::class, 'index']);
